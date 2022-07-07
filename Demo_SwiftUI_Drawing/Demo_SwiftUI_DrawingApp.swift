4763320602a167b1efcd3e9bd2f9d1426f211586
Tak Liu <tliu@connected.io>
2022-07-07 15:32:01 -0400

    first commit

---
+++ Demo_SwiftUI_Drawing/Demo_SwiftUI_DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_SwiftUI_DrawingApp.swift
+//  Demo_SwiftUI_Drawing
+//
+//  Created by Tak Liu on 2022-07-07.
+//
+
+import SwiftUI
+
+@main
+struct Demo_SwiftUI_DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
